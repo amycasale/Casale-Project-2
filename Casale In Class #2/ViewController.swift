ca5332b9642657f9d4e34a628b5f82242a99fd34
Amy Casale <a.casale@setonhill.edu>
2020-02-04 11:27:58 -0500

    Initial Commit

---
+++ Casale In Class #2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Casale In Class #2
+//
+//  Created by Amy Casale on 2/4/20.
+//  Copyright © 2020 Amy Casale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Casale In Class #2Tests/Casale_In_Class__2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Casale_In_Class__2Tests.swift
+//  Casale In Class #2Tests
+//
+//  Created by Amy Casale on 2/4/20.
+//  Copyright © 2020 Amy Casale. All rights reserved.
+//
+
+import XCTest
+@testable import Casale_In_Class__2
+
+class Casale_In_Class__2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
